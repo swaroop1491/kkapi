516149f28739434fe8f78f1eba8cfc811123630e
Swaroop Naik <snaik303@gmail.com>
2020-07-06 12:49:10 +0530

    package

--- app.js
+++ app.js
@@ -13,10 +13,5 @@ app.get('/news/:id', function (req, res, next) {
    .search(id)
    .then(resp => res.json(resp));
 });
-var server = app.listen(app.get('port'), function () {
-    var host = server.address().address
-    var port = server.address().port
- 
-    console.log("Example app listening at http://%s:%s", host, port)
- })
+var server = app.listen(app.get('port'))
 
