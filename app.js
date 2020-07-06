949bf6d2aa2d8ff06b0243b52157faf826b83e1d
Swaroop Naik <snaik303@gmail.com>
2020-07-06 13:31:20 +0530

    h

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ var cors = require('cors')
 const GoogleNewsRss = require('google-news-rss');
 
 var app = express();
-app.use(allowCrossDomain)
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 // var corsOptions = {
 //     origin: 'http://localhost:4200/',
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
