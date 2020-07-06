e62aa55e7d04fb3943827c930400e0ea1e230657
Swaroop Naik <snaik303@gmail.com>
2020-07-06 13:33:18 +0530

    h

--- app.js
+++ app.js
@@ -5,24 +5,6 @@ const GoogleNewsRss = require('google-news-rss');
 
 var app = express();
 // Add headers
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 // var corsOptions = {
 //     origin: 'http://localhost:4200/',
 //     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -32,6 +14,10 @@ app.set('port', (process.env.PORT || 8080))
 const googleNews = new GoogleNewsRss();
 
 app.get('/news/:id', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
 	var id = req.params.id;
     googleNews
    .search(id)
