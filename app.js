f1ed6b311042e9351bea390bda4fc40e4bd840a0
Swaroop Naik <snaik303@gmail.com>
2020-07-06 13:23:49 +0530

    h

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var http = require('http');
+var cors = require('cors')
 const GoogleNewsRss = require('google-news-rss');
 
 var app = express();
+var corsOptions = {
+    origin: 'http://localhost:4200/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
 app.set('port', (process.env.PORT || 8080))
 //var server = http.createServer(app);
 const googleNews = new GoogleNewsRss();
 
-app.get('/news/:id', function (req, res, next) {
+app.get('/news/:id', cors(corsOptions),function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "https://kankhabar.com/*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	var id = req.params.id;
