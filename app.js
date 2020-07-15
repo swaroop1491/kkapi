8a4c5ea532f1aa7d63b6136e9ec34165d0d76b60
Swaroop Naik <snaik303@gmail.com>
2020-07-15 21:26:00 +0530

    h

--- app.js
+++ app.js
@@ -23,5 +23,12 @@ app.get('/news/:id', function (req, res, next) {
    .search(id)
    .then(resp => res.json(resp));
 });
+app.get('/test', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+	return true;
+});
 var server = app.listen(app.get('port'))
 
