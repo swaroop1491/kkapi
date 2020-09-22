70f3e51704c479bd33c05340797f32213b10bbee
swaroop1491 <snaik303@gmail.com>
2020-09-22 12:15:28 +0000

    changes

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var http = require('http');
-var cors = require('cors')
+var cors = require('cors');
+const urlMetadata = require('url-metadata')
+const googleTrends = require('google-trends-api');
 const GoogleNewsRss = require('google-news-rss');
-
+const googleNewsScraper = require('google-news-scraper');
+const urlMetadata = require('url-metadata');
+var previewData = [];
 var app = express();
 // Add headers
 // var corsOptions = {
@@ -21,7 +25,52 @@ app.get('/news/:id', function (req, res, next) {
 	var id = req.params.id;
     googleNews
    .search(id)
-   .then(resp => res.json(resp));
+   .then(function(resp) {
+     for(let i=0;i<resp.length;i++){
+       let z = preview(resp[i].link);
+      previewData.push(z);
+     }
+     res.send(previewData);
+   } );
+});
+async preview => (url){
+  await urlMetadata(resp[i].link).then(
+      function (metadata) { // success handler
+        return metadata;
+      },
+      function (error) { // failure handler
+        console.log(error)
+      })
+};
+app.get('/trends/:id', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    res.setHeader('Content-Type', 'application/json');
+	var id = req.params.id;
+    googleTrends.relatedQueries({keyword: id})
+.then(function(r){
+  let x = r.replace(/\\/g, "");
+  console.log(x)
+  res.send(x);
+})
+});
+app.get('/preview/:id', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    res.setHeader('Content-Type', 'application/json');
+	var id = req.params.id;
+   res.send('Hello World!')
+// urlMetadata(id).then(
+//   function (metadata) { // success handler
+//     res.send(metadata);
+//   },
+//   function (error) { // failure handler
+//     console.log(error)
+//   })
 });
 app.get('/test', (req, res) => res.send('Hello World!'))
 var server = app.listen(app.get('port'))
