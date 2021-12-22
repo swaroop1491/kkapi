9b725505558f491ab56937a30b7717ba97be94a6
swaroop1491 <51492040+swaroop1491@users.noreply.github.com>
2021-12-22 21:43:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ const grabity = require("grabity");
 const googleNewsScraper = require('google-news-scraper');
 var previewData = [];
 var app = express();
-// Add headers
-// var corsOptions = {
-//     origin: 'http://localhost:4200/',
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-//   }
-//app.set('port', (process.env.PORT || 8080))
-//var server = http.createServer(app);
 const googleNews = new GoogleNewsRss();
 
 app.get('/', (req, res) => res.send('Home Page Route'));
@@ -26,6 +19,20 @@ app.get('/news/:id', function (req, res, next) {
     googleNews
    .search(id)
    .then(resp => res.send(resp) );
+});
+app.get('/trends/:id', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    res.setHeader('Content-Type', 'application/json');
+	var id = req.params.id;
+    googleTrends.relatedQueries({keyword: id})
+.then(function(r){
+  let x = r.replace(/\\/g, "");
+  console.log(x)
+  res.send(x);
+})
 });
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
