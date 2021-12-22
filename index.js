635e7059a14155ed8d6a447540c81924e2d79a6e
swaroop1491 <51492040+swaroop1491@users.noreply.github.com>
2021-12-22 21:39:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var http = require('http');
+var cors = require('cors');
+const googleTrends = require('google-trends-api');
+const GoogleNewsRss = require('google-news-rss');
+const grabity = require("grabity");
+const googleNewsScraper = require('google-news-scraper');
+var previewData = [];
+var app = express();
+// Add headers
+// var corsOptions = {
+//     origin: 'http://localhost:4200/',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+//app.set('port', (process.env.PORT || 8080))
+//var server = http.createServer(app);
+const googleNews = new GoogleNewsRss();
+
 app.get('/', (req, res) => res.send('Home Page Route'));
-app.get('/about', (req, res) => res.send('About Page Route'));
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+app.get('/news/:id', function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+	var id = req.params.id;
+    googleNews
+   .search(id)
+   .then(resp => res.send(resp) );
+});
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
